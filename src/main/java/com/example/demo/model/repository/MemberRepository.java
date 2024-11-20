42f6bcaa5b3ceae2fd61d6cca3f9bb429489b752
admin <linuloo@naver.com>
2024-11-20 13:44:10 +0900

    10주차_글수정미완

--- src/main/java/com/example/demo/model/repository/MemberRepository.java
+++ src/main/java/com/example/demo/model/repository/MemberRepository.java
@@ -4,9 +4,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.demo.model.domain.Board;
+import com.example.demo.model.domain.Member;
 
 @Repository
-public interface BoardRepository extends JpaRepository<Board, Long>{
-    Page<Board> findByTitleContainingIgnoreCase(String title, Pageable pageable);
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByEmail(String email);
 }
